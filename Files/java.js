98867c085a9b08c17ce67e6ec33eed3f27e25d53
AbdulMatin Alayande Nuhu <131428757+AlayandeWork@users.noreply.github.com>
2024-10-22 23:34:46 -0400

    Added javascript changing names

---
+++ Files/java.js
@@ -0,0 +1,21 @@
+const texts = [
+    "Data Scientist",
+    "Game Designer",
+    "UX/UI Designer",
+    "Data Analyst",
+    "Petroleum Engineer"
+]
+
+const paragraph = document.getElementById("changingText");
+
+let currentIndex = 0;
+
+function changeText() {
+
+    currentIndex = (currentIndex + 1) % texts.length;
+
+
+    paragraph.textContent = texts[currentIndex];
+}
+
+setInterval(changeText, 2000);
